7831181f54154cfe29266c51910f50e0bc461455
Adi Linden (docker-minecraft) <adi@adis.ca>
2020-05-11 01:02:29 -0500

    Added proposed environment variables.

--- bedrock-entry.sh
+++ bedrock-entry.sh
@@ -45,5 +45,28 @@ for f in $files; do
     ln -sf ${BEDROCK_INSTALL}/${f} ${BEDROCK_DATA}/${f}
 done
 
+# The following environment variables are supported and mapped to their
+# server.properties values as indicated.
+# See https://minecraft.gamepedia.com/Server.properties#Bedrock_Edition_3
+# 
+# Environment variable            server.properties               Default          Allowed values
+# --------------------            -----------------               -------          --------------
+# GAMEMODE                        gamemode                        survival         survival (0), creative (1), adventure (2)
+# DIFFICULTY                      difficulty                      easy             peaceful (0), easy (1), normal (2), hard (3)
+# LEVEL_TYPE                      level-type                      DEFAULT          FLAT, LEGACY, DEFAULT 
+# SERVER_NAME                     server-name                     Dedicated Server AAny string
+# MAX_PLAYERS                     max-players                     10               INT 
+# SERVER_PORT                     server-port                     19132            INT
+# LEVEL_NAME                      level-name                      level            Any string
+# LEVEL_SEED                      level-seed                      empty            Any string  
+# ONLINE_MODE                     online-mode                     true             true, false
+# WHITE_LIST                      white-list                      false            true, false
+# ALLOW_CHEATS                    allow-cheats                    false            true, false
+# VIEW_DISTANCE                   view-distance                   10               INT
+# PLAYER_IDLE_TIMEOUT             player-idle-timeout             30               INT
+# MAX_THREADS                     max-threads                     8                INT
+# TICK_DISTANCE                   tick-distance                   4                INT [4-12]
+# DEFAULT_PLAYER_PERMISSION_LEVEL default-player-permission-level member           visitor, member, operator
+# TEXTUREPACK_REQUIRED            texturepack-required            false            true, false
 
 
